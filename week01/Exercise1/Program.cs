904bf5086a27c8712e7c4f8a984371b99319cd54
pher9999 <criordan@hal-9000.net>
2026-02-28 20:27:20 -0600

    excerside1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your First name?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("What is your Last name?");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
