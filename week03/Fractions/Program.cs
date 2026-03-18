bd963519dd68d6b3b342975c2f6b54421dc379e9
pher9999 <criordan@hal-9000.net>
2026-03-17 20:48:40 -0500

    week3 start

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+
+        Fraction f2 = new Fraction(3, 4);
+        Console.WriteLine(f2.GetFractionString());
     }
 }
