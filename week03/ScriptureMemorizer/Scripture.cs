cb6fd6a5976647734f65d9f1aa5b7248d7f6dcf6
pher9999 <criordan@hal-9000.net>
2026-03-23 17:50:57 -0500

    start

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,6 +1,30 @@
 public class Scripture()
 {
     Reference _reference = new Reference();
+    List<Word> _words = new List<Word>();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        // Split the text into words and create Word objects
+    }
+
+    public void HideRandomWord()
+    {
+        // Randomly select a Word object and call its Hide method
+    }
+
+    public string GetDisplayText()
+    {
+        // Get the display text from the Reference and concatenate it with the display text of each Word
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        // Check if all Word objects are hidden
+    }
+    s
+
 
 
 
