5a441f10e0315005e21ac0a1007d572c43e2d12e
pher9999 <criordan@hal-9000.net>
2026-03-29 09:37:05 -0500

    youtube done

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -10,35 +10,29 @@ class Program
         Video video1 = new Video("C# Encapsulation", "Scott Burton", 391);
         Video video2 = new Video("Principle Review: Abstraction", "Bro Burton", 748);
         Video video3 = new Video("JavaScript Basics", "Alice Johnson", 450);
-
-        // Display video information
-        Console.WriteLine(video1.displayVideo());
         // Create some comments for video1
-        Comment comment1 = new Comment("User1", "Great video! Very helpful.");
-        Console.WriteLine(comment1.displayComment());
-        Comment comment2 = new Comment("User2", "I have a question about encapsulation.");
-        Console.WriteLine(comment2.displayComment());
-        Comment comment3 = new Comment("User3", "Can you explain abstraction in more detail?");
-        Console.WriteLine(comment3.displayComment());
+        video1.AddComment(new Comment("User1", "Great video! Very helpful."));
+        video1.AddComment(new Comment("User2", "I have a question about encapsulation."));
+        video1.AddComment(new Comment("User3", "Can you explain abstraction in more detail?"));
+
+        Console.WriteLine(video1.displayVideo());
+
+
+
+        video2.AddComment(new Comment("User1", "Great video! Very helpful."));
+        video2.AddComment(new Comment("User2", "I have a question about encapsulation."));
+        video2.AddComment(new Comment("User3", "Can you explain abstraction in more detail?"));
 
         Console.WriteLine(video2.displayVideo());
-        // Create some comments for video2
-        Comment comment4 = new Comment("User1", "Great video! Very helpful.");
-        Console.WriteLine(comment1.displayComment());
-        Comment comment5 = new Comment("User2", "I have a question about encapsulation.");
-        Console.WriteLine(comment2.displayComment());
-        Comment comment6 = new Comment("User3", "Can you explain abstraction in more detail?");
-        Console.WriteLine(comment3.displayComment());
 
 
+
+        video3.AddComment(new Comment("User1", "Great video! Very helpful."));
+        video3.AddComment(new Comment("User2", "I have a question about encapsulation."));
+        video3.AddComment(new Comment("User3", "Can you explain abstraction in more detail?"));
         Console.WriteLine(video3.displayVideo());
         // Create some comments for video3
-        Comment comment7 = new Comment("User1", "Great video! Very helpful.");
-        Console.WriteLine(comment1.displayComment());
-        Comment comment8 = new Comment("User2", "I have a question about encapsulation.");
-        Console.WriteLine(comment2.displayComment());
-        Comment comment9 = new Comment("User3", "Can you explain abstraction in more detail?");
-        Console.WriteLine(comment3.displayComment());
+
 
 
 
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -3,6 +3,8 @@ public class Video
     private string _title;
     private string _author;
     private int _length;
+    private List<Comment> _comment = new List<Comment>();
+    private string _displayVideo;
 
     public Video()
     {
@@ -20,12 +22,27 @@ public class Video
 
     public string displayVideo()
     {
-        return $"Title: {_title}\nAuthor: {_author}\nLength: {_length} seconds";
+        _displayVideo = $"Title: {_title}\nAuthor: {_author}\nLength: {_length} seconds\nComments ({_comment.Count()}):\n";
+        for (int i = 0; i < _comment.Count(); i++)
+        {
+            _displayVideo += _comment[i].displayComment();
+            _displayVideo += "\n";
+
+        }
+        return _displayVideo;
     }
 
     public int GetNumOfComments()
     {
         return _comment.Count();
     }
+
+    public void AddComment(Comment comment)
+    {
+        _comment.Add(comment);
+    }
+
+
+
 
 }
