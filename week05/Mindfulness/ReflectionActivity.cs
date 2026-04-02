2483b79f5d659b0110e19783f5cdeff6637b8d57
pher9999 <criordan@hal-9000.net>
2026-04-01 21:57:39 -0500

    group meeting done

--- week05/Mindfulness/ReflectionActivity.cs
+++ week05/Mindfulness/ReflectionActivity.cs
@@ -1,5 +1,28 @@
 public class ReflectionActivity : Activity
 {
+
+    private List<string> _prompts = new List<string>
+    {
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think od a thime when you helped someone in need.",
+        "Think of a time when you did something truly selfless."
+
+    };
+
+    private List<string> _questions = new List<string>
+    {
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you get started?",
+        "How did you feel when it was complete?",
+        "What made this time different than other times when you were not as successful?",
+        "What is your favorite thing about this experience?",
+        "What could you learn from this experience that applies to other situations?",
+        "What did you learn about yourself through this experience?",
+        "How can you apply what you learned from this experience in the future?"
+    };
+
     public ReflectionActivity(string activityName, string description) : base(activityName, description)
     {
         _activityName = activityName;
