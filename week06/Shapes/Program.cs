1819eb5ef2c65c6d8527b8eea25b0481c52a8e65
pher9999 <criordan@hal-9000.net>
2026-04-05 16:37:17 -0500

    week6

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+        Rectangle r1 = new Rectangle("Blue", 4, 5);
+        shapes.Add(r1);
+        Circle circle1 = new Circle("Green", 2);
+        shapes.Add(circle1);
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+
+            Console.WriteLine($"Shape: {color} shape has an area of {area}");
+        }
     }
 }
